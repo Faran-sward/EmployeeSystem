d7c2fb67bf8f6b354b10ff8a49a106df3bda260e
aoeas771H <134822907+aoeas771H@users.noreply.github.com>
2023-07-05 09:40:30 +0800

    Add files via upload

---
+++ C#Learn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace My_TEST
+{
+    class _test_
+    {
+        string str = @"test-
+                    123 456-
+                    444";
+        public _test_()
+        {
+            Console.WriteLine("构造函数");
+        }
+        public void _print(string str)
+        {
+            Console.WriteLine(str);
+            Console.WriteLine(str.Length);
+        }
+    }
+}
