523d0683dadfeb0d546959fccfae8054b55a2feb
aoeas771H <134822907+aoeas771H@users.noreply.github.com>
2023-07-26 16:23:53 +0800

    Add files via upload

---
+++ 黄露/Controllers/BasicInfoController.cs
@@ -0,0 +1,59 @@
+﻿using DB_project.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DB_project.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BasicInfoController : ControllerBase
+    {
+        private static string connectionString = "Data Source=localhost/ORCL;User ID=sys_developer;Password=1738477932;";
+        [HttpGet("{EmpID}")]
+        public string GetBasicInfo(string EmpID)
+        {
+            List<BasicInfo> BasicInfos = new List<BasicInfo>();
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM FundamentalInfo F, EmployeeInfo E WHERE f.employeeid = e.employeeid and f.EMPLOYEEID= :EmpID";
+                    BasicInfo basicInfo = new BasicInfo();
+                    using (OracleCommand cmd = new OracleCommand(sql, connection))
+                    {
+                        cmd.Parameters.Add(":EmpID", OracleDbType.Varchar2).Value = EmpID;
+                        using (OracleDataReader reader = cmd.ExecuteReader())
+                        {
+                            reader.Read();
+                            // 根据需要获取列的值
+                            basicInfo.ID = reader["ID_number"].ToString();
+                            basicInfo.Name = reader["Name"].ToString();
+                            basicInfo.BasicSalary = reader["BasicSalary"].ToString();
+                            basicInfo.Sex = reader["Sex"].ToString();
+                            basicInfo.PhoneNumber = reader["PhoneNumber"].ToString();
+                            basicInfo.HomeAddress = reader["HomeAddress"].ToString();
+                            basicInfo.ImageURL = reader["ImageURL"].ToString();
+                            basicInfo.WorkState = reader["WorkState"].ToString();
+                            string sTime = reader["StartWorkingDate"].ToString();
+                            string birth = reader["Birth"].ToString();
+                            string startTime = sTime.Substring(0, sTime.Length - 8);
+                            string birthTime = birth.Substring(0, birth.Length - 8);
+                            basicInfo.Birth = birthTime;
+                            basicInfo.StartWorkingDate = startTime;
+                            BasicInfos.Add(basicInfo);
+                        }
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("连接或操作数据库时发生错误：" + ex.Message);
+                }
+            }
+            return JsonConvert.SerializeObject(BasicInfos);
+        }
+    }
+}
---
+++ 黄露/Model/BasicInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace DB_project.Model
+{
+    public class BasicInfo
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string StartWorkingDate { get; set; }
+        public string BasicSalary { get; set;}
+        public string Sex { get; set;}
+        public string Birth { get; set;}
+        public string PhoneNumber { get; set;}
+        public string HomeAddress { get; set;}
+        public string ImageURL { get; set;}
+        public string WorkState { get; set;}
+    }
+}
