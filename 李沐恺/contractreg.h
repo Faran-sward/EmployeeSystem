6abe3f62b9cdd6490996f5e39132cfa6b7369ec1
2154072limukai <138638906+2154072limukai@users.noreply.github.com>
2023-08-02 23:02:18 +0800

    Add files via upload

---
+++ 李沐恺/contractreg.h
@@ -0,0 +1,58 @@
+﻿#ifndef CONTRACTREG_H
+#define CONTRACTREG_H
+
+#include <QWidget>
+#include <QString>
+#include <QDebug>
+#include <QString>
+#include <QLayout>
+#include <QCalendarWidget>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QTextCodec>
+
+namespace Ui {
+class ContractReg;
+}
+
+class ContractReg : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ContractReg(QWidget *parent = nullptr);
+    ~ContractReg();
+
+signals:
+    void ToContractInfo();
+
+public:
+    void ModifyId();
+    void encode_url(QString str, QByteArray &outArr);
+
+private slots:
+    void on_lineEdit_editingFinished();
+
+    void on_lineEdit_2_editingFinished();
+
+    void on_lineEdit_3_editingFinished();
+
+    void on_lineEdit_4_editingFinished();
+
+    void on_lineEdit_5_editingFinished();
+
+    void on_lineEdit_6_editingFinished();
+
+public slots:
+    void contract_upload(void);
+
+private:
+    Ui::ContractReg *ui;
+    QString new_employee_id,new_contract_id,new_contract_type
+        ,new_effective_date,new_inoperative_date,new_document_link;
+};
+
+#endif // CONTRACTREG_H
